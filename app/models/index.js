c5a25e919cade7c7797cd43437996cea71b144c4
Nora Neko <bayurendi10@gmail.com>
2022-12-28 10:35:00 +0700

    move config to env

--- app/models/index.js
+++ app/models/index.js
@@ -6,10 +6,10 @@ const Sequelize = require("sequelize");
 const process = require("process");
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || "development";
-const config = require(__dirname + "/../config/config.json")[env];
+const config = require(__dirname + "/../config/config.js")[env];
 const User = require("./user.js");
 const db = {};
-
+console.log(config);
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
